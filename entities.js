5dfb3fd83e014df94caaa7570bae1d54d467a722
Pifary-dev <pifary.dev@gmail.com>
2024-03-02 20:35:50 +0300

    Chrono changes
    More consistent frames

--- entities.js
+++ entities.js
@@ -59,7 +59,7 @@ class Entity {
     }
     if(game.players[0].className == "Chrono" && this.isEnemy){
       this.teleportPosition.push(new Vector(this.pos.x,this.pos.y));
-      if(this.teleportPosition.length>75/timeFix){this.teleportPosition.shift();}
+      if(this.teleportPosition.length>Math.round(75/timeFix)){this.teleportPosition.shift();}
     }
     var dim = 1 - this.friction;
     this.vel.x *= dim;
@@ -1385,7 +1385,7 @@ class Chrono extends Player {
   abilities(time, area, offset) {
     const timeFix = time / (1000 / 30);
     this.teleportPosition.push(new Vector(this.pos.x,this.pos.y));
-    if(this.teleportPosition.length>75/timeFix){this.teleportPosition.shift();}
+    if(this.teleportPosition.length>Math.round(75/timeFix)){this.teleportPosition.shift();}
 
     if (this.firstAbility && this.firstAbilityCooldown == 0 && this.energy >= 30) {
       this.firstAbilityActivated = !this.firstAbilityActivated;
@@ -1837,7 +1837,7 @@ class Enemy extends Entity {
     var timeFix = time / (1000 / 30);
     if(game.players[0].className == "Chrono"){
       this.teleportPosition.push(new Vector(this.pos.x,this.pos.y));
-      if(this.teleportPosition.length>75/timeFix){this.teleportPosition.shift();}
+      if(this.teleportPosition.length>Math.round(75/timeFix)){this.teleportPosition.shift();}
     }
     var vel = new Vector(this.vel.x * this.speedMultiplier, this.vel.y * this.speedMultiplier)
     this.speedMultiplier = 1;
